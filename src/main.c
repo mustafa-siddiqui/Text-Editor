905cd5cc16db4ebcda5f090bcb91418ad56da92f
Mustafa Siddiqui <mustafa.eins@gmail.com>
2022-01-30 17:42:56 +0500

    init repo

---
+++ src/main.c
@@ -0,0 +1,12 @@
+/** @file   main.c
+ *  @brief  Main driver code for text-editor.
+ *  @author Mustafa Siddiqui & Hamza Siddiqui
+ */
+
+ #include <stdio.h>
+
+ int main(void) {
+
+    return 0;
+ }
+
