06ee5f50d1b4d8594eb28ff680765e36f85c90b7
Hamza Siddiqui <66068932+hamzaasiddiqui@users.noreply.github.com>
2022-02-05 15:44:15 +0500

    Add files via upload
    updated files

--- src/main.c
+++ src/main.c
@@ -6,26 +6,30 @@
 #include <stdio.h>
 #include <unistd.h>  // STDIN_FILENO
 #include <ctype.h>   // iscntrl()
+#include <errno.h>   //errrno
 //-//
 #include "../inc/terminal.h"
+
+/* macro to mirror ctrl key functionality
+bitwise-ANDs 'key' character with 00011111
+to map character to bytes 1-26*/
+#define CTRL_KEY(key) ((key) & 0x1f)
 
 int main() {
    enableRawMode();
 
    while (1) {
       char c = '\0';
-      read(STDIN_FILENO, &c, 1);
+      if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die ("read");
 
       // check if character is a control key
       if (iscntrl(c)) {
-         // detect ctrl + c
-         if (c == 3)
-            break;
          printf("%d\r\n", c);
       }
       else {
          printf("%c\r\n", c);
       }
+      if (c == CTRL_KEY('c')) break;
    }
    
    return 0;
